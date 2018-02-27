f36441af1bc0c84d0724a16e5a8d3b5c5d131c00
Garrett Ransom <g2ransom@gmail.com>
2018-02-26 19:21:19 -0500

    cleaned script for active users pipeline

--- traction_stats/storageevents_stats.py
+++ traction_stats/storageevents_stats.py
@@ -1,22 +1,58 @@
-from pymongo import MongoClient
-import pandas as pd
-from bson.son import SON
+users_pipeline = [
+	{
+		"$group": {
+		"_id": {
+		"month": {"$month": "$timestamp"}, 
+		"year": {"$year": "$timestamp"}
+		}, 
+		"users": {"$addToSet": "$user"},
+		}
+	},
+	{"$sort": SON([("_id", 1)])},
+	{
+		"$project": {
+		"_id": 0, 
+		"month": "$_id.month", 
+		"year": "$_id.year", 
+		"active_users": {"$size": "$users"},
+		}
+	}
+	
+]
 
-def storage_events_df():
-	client = MongoClient()
+# def storage_events_df():
+# 	client = MongoClient(host='104.196.54.95',
+# 						port=27017,
+# 						ssl=True,
+# 						ssl_cert_reqs=ssl.CERT_NONE,
+# 						username='root',
+# 						password='kM8tRoRtQDBFBkN9ZOFNYR5'
+# 						)
 
-	db = client['test']
+# 	db = client['bridge']
 
-	pipeline = [
-		{"$group": {"_id": {"month": {"$month": "$timestamp"}, "year": {"$year": "$timestamp"}}, 
-		"user": {"$addToSet": "$user"}}},
-		{"$sort": SON([("_id", 1)])},
-		{"$project": {"_id": 0, "month": "$_id.month", "year": "$_id.year", "userbyMonth": {"$size": "$user"}}}
+# 	pipeline = [
+# 		{"$group": 
+# 			{"_id": 
+# 				{"month": {"$month": "$timestamp"}, 
+# 				"year": {"$year": "$timestamp"}}, 
+# 				"user": {"$addToSet": "$user"}
+# 			}
+# 		},
+# 		{"$sort": SON([("_id", 1)])},
+# 		{"$project": 
+# 			{"_id": 0, 
+# 			"month": "$_id.month", 
+# 			"year": "$_id.year", 
+# 			"userbyMonth": {"$size": "$user"}
+# 			}
+# 		}
 		
-	]
+# 	]
 
-	usersbyMonth = list(db.storageevents.aggregate(pipeline))
+# 	# change db to correct name, if not correct
+# 	usersbyMonth = list(db.storageevents.aggregate(pipeline))
 
-	storageDataFrame = pd.DataFrame(usersbyMonth)
-	storageDataFrame = storageDataFrame[['month', 'year', 'userbyMonth']]
-	return storageDataFrame
+# 	storageDataFrame = pd.DataFrame(usersbyMonth)
+# 	storageDataFrame = storageDataFrame[['month', 'year', 'userbyMonth']]
+# 	return storageDataFrame
