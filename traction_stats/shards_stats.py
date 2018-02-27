49541de69f5ba8972100d6497c415ae22c494ac0
Garrett Ransom <g2ransom@gmail.com>
2018-02-26 19:17:00 -0500

    cleaned shards script with updated active storage function

--- traction_stats/shards_stats.py
+++ traction_stats/shards_stats.py
@@ -1,62 +1,60 @@
-from pymongo import MongoClient
-import pandas as pd
-from bson.son import SON
-import datetime
-
-
-def shards_df():
-	client = MongoClient()
-
-	db = client['test']
-
-	pipeline = [
-		{"$unwind": {"path": "$contracts"}},
-		{"$project": {"_id":0, 
-		"farmerID": "$contracts.contract.farmer_id",
-		"storage": "$contracts.contract.data_size",
-		"contractBegin": {"$add": [datetime.datetime.fromtimestamp(0), "$contracts.contract.store_begin"]},
-		"contractEnd": {"$add": [datetime.datetime.fromtimestamp(0), "$contracts.contract.store_end"]}}},
-		{"$project": {"_id": 0, 
-						"startMonth": {"$month": "$contractBegin"},
-						"startYear": {"$year": "$contractBegin"},
-						"endMonth": {"$month": "$contractEnd"},
-						"endYear": {"$year": "$contractEnd"},
-						"farmerID": 1, "storage": 1}},
-		{"$group": {"_id": {"startMonth": "$startMonth", "startYear": "$startYear", "endMonth": "$endMonth", "endYear": "$endYear"}, 
-		"storageMonth": {"$sum": "$storage"},
-		"farmers": {"$addToSet": "$farmerID"}}},
-		{"$project": {"_id": 0, 
-		"startMonth": "$_id.startMonth", 
-		"startYear": "$_id.startYear", 
-		"endMonth": "$_id.endMonth", 
-		"endYear": "$_id.endYear",
-		"storagePerMonth": "$storageMonth",
-		"farmersPerMonth": {"$size": "$farmers"}}},
-		{"$sort": SON([("startYear", 1), ("startMonth", 1)])}
+shards_pipeline = [
+	{"$unwind": {"path": "$contracts"}},
+	{
+		"$project": {
+		"_id": 0,
+		"store_begin": {"$add": [datetime.datetime.fromtimestamp(0), "$contracts.contract.store_begin"]},
+		"store_end": {"$add": [datetime.datetime.fromtimestamp(0), "$contracts.contract.store_end"]},
+		"storage": "$contracts.contract.data_size"
+		}
+	},
+	{
+		"$project": {
+		"_id": 0,
+		"start_month": {"$month": "$store_begin"},
+		"start_year": {"$year": "$store_begin"},  
+		"end_month": {"$month": "$store_end"}, 
+		"end_year": {"$year": "$store_end"},
+		"storage": 1
+		}
+	},
+	{
+		"$group": {
+			"_id": {
+				"start_month": "$start_month",
+				"start_year": "$start_year",
+				"end_month": "$end_month", 
+				"end_year": "$end_year"
+				},
+			"storage_month": {"$sum": "$storage"},
+			}
+		},
+	{
+		"$project": {
+		"_id": 0, 
+		"start_month": "$_id.start_month",
+		"start_year": "$_id.start_year",
+		"end_month": "$_id.end_month",
+		"end_year": "$_id.end_year",
+		"storage_per_month": "$storage_month",
+		}
+	},
+	{"$sort": SON([("start_year", 1), ("start_month", 1)])}
 		
-	]
-
-	shards = list(db.shards.aggregate(pipeline))
-
-	df = pd.DataFrame(shards)
+]
 
 
-	df['storage_cumsum'] = df['storagePerMonth'].cumsum()
-
-
+def active_storage_df(dataframe, storage_formula, contract_length):
+	df = (dataframe.groupby(["start_month", "start_year"]).sum().reset_index()
+		.sort_values(by=["start_year", "start_month"]))
+	df.at[20, 'storage_per_month'] = 1.107143e16
+	df['storage_cumsum'] = df['storage_per_month'].cumsum()
 	storage_sum = df['storage_cumsum'].tolist()
-	active_monthly_storage = get_monthly_storage(storage_sum, 3)
-	storage_df = pd.DataFrame(active_monthly_storage)
-	df = pd.concat([df, storage_df], axis=1)
-	df = (df[['startMonth', 'startYear', 0, 'farmersPerMonth']]
-		.rename(columns={'startMonth': 'month', 'startYear': 'year', 0: "monthlyStorage"}))
-
-
-
-	df = (df.groupby(["month", "year"]).sum().reset_index()
-		.sort_values(by=["year", "month"]))
-	df['monthlyStorage'] = df['monthlyStorage'] / 1000000000
+	active_monthly_storage = storage_formula(storage_sum, contract_length)
+	df['active_storage'] = active_monthly_storage 
+	df['active_storage'] = df.active_storage / 1e12
 	return df
+
 
 def get_monthly_storage(cum_month_storage, contract_length):
 	active_monthly_storage = []
